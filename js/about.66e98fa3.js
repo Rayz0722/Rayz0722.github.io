6389a8ccb1dcc3ef708bb89605dfdb64571841b2
Ruiyang Zhou <rz3zv@virginia.edu>
2024-09-05 18:36:58 -0400

    Deploy Vue.js app to GitHub Pages

---
+++ js/about.66e98fa3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmy_web"]=self["webpackChunkmy_web"]||[]).push([[594],{7833:function(e,r,n){n.r(r),n.d(r,{default:function(){return p}});var u=n(6768);const s={class:"Resume"},t=["src"];function a(e,r,n,a,c,f){return(0,u.uX)(),(0,u.CE)("div",s,[(0,u.Lk)("iframe",{src:c.pdfUrl,width:"70%",height:"1000px"},null,8,t)])}var c=n.p+"assets/pdf/Ruiyang_Zhou_CV.3c83f419.pdf",f={data(){return{pdfUrl:c}}},d=n(1241);const i=(0,d.A)(f,[["render",a]]);var p=i}}]);
+//# sourceMappingURL=about.66e98fa3.js.map
