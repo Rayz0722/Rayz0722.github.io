8d6634cf1116b8f77550469ed74bd3fd13fcb6c8
Ruiyang Zhou <rz3zv@virginia.edu>
2025-07-29 17:32:40 +0800

    use hashrouter to avoid 404 routing on Github

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import CV from './pages/CV';
-import Music from './pages/Music';
-import Tennis from './pages/project_page/Tennis';
-import LostFound from './pages/project_page/LostFound';
-import MultibandDistortion from './pages/project_page/MultibandDistortion';
-import FMSynthesizer from './pages/project_page/FMSynthesizer';
-import SongSeg from './pages/project_page/SongSeg';
-import './App.css'
-
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import CV from "./pages/CV";
+import Music from "./pages/Music";
+import Tennis from "./pages/project_page/Tennis";
+import LostFound from "./pages/project_page/LostFound";
+import MultibandDistortion from "./pages/project_page/MultibandDistortion";
+import FMSynthesizer from "./pages/project_page/FMSynthesizer";
+import SongSeg from "./pages/project_page/SongSeg";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <nav>
-        <Link to="/" >Home</Link>
+        <Link to="/">Home</Link>
         <Link to="/Projects">Projects</Link>
         <Link to="/Music">Music</Link>
         <Link to="/CV">CV</Link>
-
       </nav>
 
       <Routes>
@@ -29,17 +26,18 @@ function App() {
         <Route path="/Music" element={<Music />} />
         <Route path="/CV" element={<CV />} />
 
-
         {/* Subroutes for each project */}
         <Route path="/projects/song-segmentation" element={<SongSeg />} />
         <Route path="/projects/tennis" element={<Tennis />} />
         <Route path="/projects/lost-found" element={<LostFound />} />
-        <Route path="/projects/multiband-distortion" element={<MultibandDistortion />} />
+        <Route
+          path="/projects/multiband-distortion"
+          element={<MultibandDistortion />}
+        />
         <Route path="/projects/fmsynthesizer" element={<FMSynthesizer />} />
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
-export default App
+export default App;
