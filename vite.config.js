7f62dd57d13f334fe802825aa1a65c3eb9ec96fc
Ruiyang Zhou <rz3zv@virginia.edu>
2025-07-29 16:58:31 +0800

    set up deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Rayz0722.github.io/", // or repo name
+  base: "/",
 });
